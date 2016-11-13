d9698e008b7e0499568fe18437e48cb4d0cf2c0b
Zach Bullock <bullock.zd@gmail.com>
2016-11-12 20:35:25 -0600

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return(
-    <h3>Examples Component 2!!</h3>
+    <div>
+      <h3>Examples Component 2!!</h3>
+      <p>Welcome to examples!!!</p>
+    </div>
   )
 };
 
